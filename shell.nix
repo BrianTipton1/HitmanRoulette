1cea1582c1315c5bd954c880c10eb474f7ca493e
Brian Tipton <briantiptondev@gmail.com>
2022-11-03 17:12:11 -0500

    nix dev setup files

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  my-python = pkgs.python3;
+  node = pkgs.nodejs;
+  python-with-my-packages = my-python.withPackages (p: with p; [
+   python-lsp-server
+  ]);
+in
+pkgs.mkShell {
+  buildInputs = [
+    python-with-my-packages
+    node
+  ];
+  shellHook = ''
+    PYTHONPATH=${python-with-my-packages}/${python-with-my-packages.sitePackages}
+    # maybe set more env-vars
+  '';
+}
